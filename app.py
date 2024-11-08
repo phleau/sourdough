c5bcd5cb7cb766ee07fb05369a65f012458c6f59
phleau <flolabec@gmail.com>
2024-11-08 13:51:58 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ def bereken_watertemperatuur(omgeving_en_meel_temp, gewenste_deeg_temp, frictie_
     return 3 * gewenste_deeg_temp - omgeving_en_meel_temp * 2 - frictie_factor
 
 st.title("Calculator: water temperature for making sourdough bread")
-st.write("Determine the optimal temperature for your water")
-st.write("Assuming the room and flour temperature are the same:")
+
+# Use markdown with HTML to adjust font size
+st.markdown("<h3>Determine the optimal temperature for your water</h3>", unsafe_allow_html=True)
+st.markdown("<h4>Assuming the room and flour temperature are the same:</h4>", unsafe_allow_html=True)
 
 # Eén inputveld voor zowel kamertemperatuur als meeltemperatuur
 omgeving_en_meel_temp = st.number_input("Room and flour temperature (°C)", min_value=-20, max_value=50, value=20)
