92beadce9fdd37092779807f0754d392b388645d
phleau <flolabec@gmail.com>
2024-11-08 13:26:05 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 import streamlit as st
 
-def bereken_watertemperatuur(omgeving_temp, meel_temp, gewenste_deeg_temp):
-    return 3 * gewenste_deeg_temp - omgeving_temp - meel_temp
+def bereken_watertemperatuur(omgeving_en_meel_temp, gewenste_deeg_temp, frictie_factor):
+    return 3 * gewenste_deeg_temp - omgeving_en_meel_temp * 2 - frictie_factor
 
-st.title("Watertemperatuur Calculator voor Zuurdesem")
-st.write("Vul de gegevens in om de optimale watertemperatuur voor je zuurdesemstarter te berekenen.")
+st.title("What temperature should the water be?")
+st.write("When making sourdough bread, you can use this calculator to determine the optimal temperature for your water.")
+st.write("Assuming the room and flour temperature are the same:")
 
-omgeving_temp = st.number_input("Omgevingstemperatuur (°C)", min_value=-20, max_value=50, value=20)
-meel_temp = st.number_input("Meeltemperatuur (°C)", min_value=-20, max_value=50, value=22)
-gewenste_deeg_temp = st.number_input("Gewenste Deegtemperatuur (°C)", min_value=-20, max_value=50, value=25)
+# Eén inputveld voor zowel kamertemperatuur als meeltemperatuur
+omgeving_en_meel_temp = st.number_input("Room and flour temperature (°C)", min_value=-20, max_value=50, value=20)
+gewenste_deeg_temp = st.number_input("Desirable dough temperature (°C)", min_value=-20, max_value=50, value=25)
 
-if st.button("Bereken Watertemperatuur"):
-    watertemperatuur = bereken_watertemperatuur(omgeving_temp, meel_temp, gewenste_deeg_temp)
-    st.success(f"De benodigde watertemperatuur is: {watertemperatuur:.2f}°C")
+# Keuze voor frictiefactor (handmatig of machine)
+methode = st.radio("Mixing method", ('Hand', 'Machine'))
+frictie_factor = 1 if methode == 'Hand' else 7
+
+if st.button("Calculate!"):
+    watertemperatuur = bereken_watertemperatuur(omgeving_en_meel_temp, gewenste_deeg_temp, frictie_factor)
+    st.success(f"The water should be: {watertemperatuur:.2f}°C")
