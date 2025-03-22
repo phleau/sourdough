220624396db391eaa05634e5fb9474d9f7d5fa65
phleau <flolabec@gmail.com>
2025-03-22 22:07:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 import streamlit as st
 
-st.set_page_config(
-  page_title="BAKE STUDIO FLO", 
-   **name="Bake Studio Flo"**
-)
-
-st.title("Welcome to Bake Studio Flo")
-st.markdown("A weekly friends & family neighbourhood bakery. See the side for what I'll be baking this week, what I baked before. Pickup is always Saturday morning, pay by tikkie")
+# Immediately redirect to your real homepage
+st.switch_page("pages/bake_studio_flo.py")
