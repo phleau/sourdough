03d6272af4c0cebbd19bdd4b3b3150ca52d08809
phleau <flolabec@gmail.com>
2025-03-22 22:04:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.set_page_config(page_title="BAKE STUDIO FLO", page_icon="🥐")
+st.set_page_config(
+  page_title="BAKE STUDIO FLO", 
+   **name="Bake Studio Flo"**
+)
 
 st.title("Welcome to Bake Studio Flo")
 st.markdown("A weekly friends & family neighbourhood bakery. See the side for what I'll be baking this week, what I baked before. Pickup is always Saturday morning, pay by tikkie")
