14c0475604af2f7120fbfc41fe973b778666a4af
phleau <flolabec@gmail.com>
2025-03-28 22:09:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-import streamlit as st
+import streamlit as st.
+st.set_page_config(page_title="Redirecting...", layout="wide", initial_sidebar_state="collapsed")
 
+if "redirected" not in st.session_state:
+    st.session_state.redirected = True
+    st.switch_page("Bake Studio Flo")
+
+st.write("Redirecting...")
 
