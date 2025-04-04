424b94f4eb207d36f967713f4e7204e0106c2f95
phleau <flolabec@gmail.com>
2025-04-04 16:56:41 +0200

    Update bake_studio_flo.py

--- pages/bake_studio_flo.py
+++ pages/bake_studio_flo.py
@@ -1,5 +1,18 @@
 import streamlit as st
 
-st.set_page_config(page_title="BAKE STUDIO FLO")
+st.set_page_config(page_title="BAKE STUDIO FLO", page_icon="🥖")
 
-st.title("Welcome to Bake Studio Flo")
+st.title("Hi friends!")
+
+st.write("""
+Hi friends, thanks for being part of this neighbourhood bakery
+
+I’ve set up this little page to help with orders, not because I want to grow,  
+but because I want to keep things flowing nicely.  
+I still love hearing from you, and your messages won’t stop meaning something,  
+this is just a small experiment to keep things smooth (for both of us).
+
+Let’s see if I can code my way to a more relaxed baking rhythm 😉  
+Thanks for being part of this, you're the reason it works at all 🧡
+""")
+
