be725b48144e39330ea0b166f6fc3c40841c531e
phleau <flolabec@gmail.com>
2025-04-09 20:51:58 +0200

    Update bakes.py

--- pages/bakes.py
+++ pages/bakes.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.markdown("""
 **This Saturday (12th of April), doing a small pick-up session! You're welcome between 10h30-11h30**
 
-- 🥧 *Lemon & bergamot meringue tartelette* — **4€** (dairy free)  
+- 🥧 Lemon & bergamot meringue tartelette — **4€** (dairy free)  
   _Ingredients:_ *flour, sugar, plant-based butter, eggs, lemon, bergamot, salt (all organic)*  
 
-- 🌿 *Wild garlic focaccia* — **4€** (vegan)  
+- 🌿 Wild garlic focaccia* — **4€** (vegan)  
   _Ingredients:_ *flour, olive oil, salt, foraged wild garlic (all organic)*
 """)
