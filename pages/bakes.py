c1b0831be7b3a15e1a35817f652bc8f4093816de
phleau <flolabec@gmail.com>
2025-04-09 20:45:24 +0200

    Update bakes.py

--- pages/bakes.py
+++ pages/bakes.py
@@ -1,1 +1,15 @@
+import streamlit as st
 
+st.set_page_config(page_title="This week's bakes", page_icon="🥖")
+
+st.title("Hi lovely people!")
+
+st.write("""
+  
+  This Saturday (12th of April), doing a small pick-up session! 
+  - Lemon & bergamot meringue tartelette (4 euro) (dairy free)
+      Ingredients: flour, sugar, plant-based butter, eggs, lemon, bergamot, salt (all organic)
+      
+  - Wild garlic focaccia (4 euro) (vegan)
+      Ingredients: flour, olive oil, salt, foraged wild garlic (all organic)
+"")
